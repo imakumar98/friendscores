b569a0725d29ae4d2d7897b081bced49c10663ca
Ashwani <imakumar98@gmail.com>
2020-01-14 15:44:52 +0530

    project complete

--- includes/config.php
+++ includes/config.php
@@ -2,10 +2,10 @@
 
 	//DEFINE ALL THE CONFIGURATION SETTINGS
 	DEFINE('HOST', 'localhost');
-	DEFINE('USERNAME', 'friend_score_db');
-	DEFINE('PASSWORD','Akshay!@34');
-	DEFINE('DB_NAME','jwtqousl_quiz');
-	DEFINE('EMAIL', 'malhotraakshay1993@gmail.com');
+	DEFINE('USERNAME', '');
+	DEFINE('PASSWORD','');
+	DEFINE('DB_NAME','');
+	DEFINE('EMAIL', '');
 	DEFINE('APP_URL', 'https://friendscores.com');
 	DEFINE('MIN_QUESTION_ID','24');
 	DEFINE('MAX_QUESTION_ID','55');
